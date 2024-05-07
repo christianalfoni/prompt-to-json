852ad20d8d3068eee3e4bc85dbee080fcce82942
Christian Alfoni <christianalfoni@gmail.com>
2024-05-07 14:26:48 +0200

    allow arrays

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ExtractValueType, ObjectSchema } from "./schema";
+import { ArraySchema, ExtractValueType, ObjectSchema } from "./schema";
 
 import { anyOf, string, array, boolean, object, enums } from "./schema";
 
@@ -11,7 +11,10 @@ export const schema = {
   enums,
 };
 
-function createJsonPrompt(prompt: string, json: ObjectSchema<any, any>) {
+function createJsonPrompt(
+  prompt: string,
+  json: ObjectSchema<any, any> | ArraySchema<any>
+) {
   return `# How to respond to this prompt
   
 ## Output schema
@@ -40,11 +43,9 @@ ${prompt}
 It is critical that you output a single valid JSON object with no markdown, extraneous text or wrappers.`;
 }
 
-export async function promptToJson<T extends ObjectSchema<any, any>>(
-  prompt: string,
-  json: T,
-  sendPrompt: (prompt: string) => Promise<string>
-) {
+export async function promptToJson<
+  T extends ObjectSchema<any, any> | ArraySchema<any>,
+>(prompt: string, json: T, sendPrompt: (prompt: string) => Promise<string>) {
   const response = await sendPrompt(createJsonPrompt(prompt, json));
 
   return JSON.parse(response) as ExtractValueType<T>;
